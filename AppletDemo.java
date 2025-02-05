9eaa20541ced4eafa08abcdf6b821484796c05a8
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-05 16:23:33 +0530

    [ADDED] : Lalit - Demonstration of Applet in Java.

---
+++ AppletDemo.java
@@ -0,0 +1,15 @@
+import java.applet.*;
+import java.awt.*;
+
+public class AppletDemo extends Applet
+{
+	public static void main(String args[])
+	{
+		
+	}
+
+	public void paint(Graphics g)
+	{
+		g.drawString("HelloS",100,100);
+	}
+}
